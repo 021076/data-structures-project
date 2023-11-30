4b06aeb672fd1c25187ec9dae2d53e2c0358877f
ro_k_ <ro_k_sana@mail.ru>
2023-11-30 23:09:19 +0500

    4 курс. DZ homework-1

--- src/stack.py
+++ src/stack.py
@@ -1,34 +1,49 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data):
         """
         Конструктор класса Node
-
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = None
 
+
+# если init так, то не будет работать n2 = Node('a', n1)
+# если в аргументы init добавить next_node и инициализировать: self.next_node = next_node, тогда push не работает
 
 class Stack:
     """Класс для стека"""
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
         Метод для добавления элемента на вершину стека
-
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        if self.top is None:
+            self.top = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next_node = self.top
+            self.top = new_node
 
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
-
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+        elif self.top.next_node is None:
+            removed = self.top.data
+            self.top = None
+            return removed
+        else:
+            removed = self.top.data
+            self.top = self.top.next_node
+            return removed
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class TestStack(unittest.TestCase):
+    def test_node(self):
+        self.assertEqual((Node(5, None)).data, 5)
+        self.assertEqual((Node('a', 5)).data, 'a')
+
+    def test_push_stack(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data1')
+
+    def test_pop_stack(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        removed = stack.pop()
+        self.assertEqual(stack.top.data, 'data1')
+        self.assertEqual(removed, 'data2')

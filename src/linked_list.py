be0861ba1323009502e48819d3a5d403444fe455
ro_k_ <ro_k_sana@mail.ru>
2023-12-07 23:11:54 +0500

    4 курс. DZ homework-5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,29 +1,47 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None  # указатель на следующий узел
+
+    def __str__(self):
+        return str(self.data)
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = None
+            return None
+        else:
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+        last_node.next = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
         node = self.head
         if node is None:
             return str(None)
-
-        ll_string = ''
+        ll_string = ""
         while node:
-            ll_string += f' {str(node.data)} ->'
-            node = node.next_node
-
-        ll_string += ' None'
+            ll_string += f'{str(node.data)}-> '
+            node = node.next
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList, Node
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_node(self):
+        node = Node({'data0': 0})
+        self.assertEqual(str(type(node)), "<class 'src.linked_list.Node'>")
+        self.assertEqual(str(node), "{'data0': 0}")
+
+    def test_llist(self):
+        ll = LinkedList()
+        self.assertEqual(str(type(ll)), "<class 'src.linked_list.LinkedList'>")
+
+    def test_insert(self):
+        ll = LinkedList()
+        self.assertEqual(str(ll), "None")
+        ll.insert_beginning({'id': 1})
+        self.assertEqual(str(ll), "{'id': 1}-> None")
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(str(ll), "{'id': 0}-> {'id': 1}-> None")
+        ll.insert_at_end({'id': 2})
+        self.assertEqual(str(ll), "{'id': 0}-> {'id': 1}-> {'id': 2}-> None")
+        ll.insert_at_end({'id': 3})
+        self.assertEqual(str(ll), "{'id': 0}-> {'id': 1}-> {'id': 2}-> {'id': 3}-> None")
